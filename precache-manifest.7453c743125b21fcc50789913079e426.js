4c7f535dea1aaa6d43c3e2a0765f9b866d059ca0
Fábio Sousa <fabiojdev@gmail.com>
2020-02-24 19:46:46 -0300

    Updates

---
+++ precache-manifest.7453c743125b21fcc50789913079e426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b60e10b0183235df0afa973d0b3c8ff",
+    "url": "/fabiojsousa/simple-app/index.html"
+  },
+  {
+    "revision": "8eb8125046bfeb071b53",
+    "url": "/fabiojsousa/simple-app/static/css/2.2623327d.chunk.css"
+  },
+  {
+    "revision": "8eb8125046bfeb071b53",
+    "url": "/fabiojsousa/simple-app/static/js/2.4b768bcd.chunk.js"
+  },
+  {
+    "revision": "c9a73602a672ed33b1d7",
+    "url": "/fabiojsousa/simple-app/static/js/main.f1bf8ec7.chunk.js"
+  },
+  {
+    "revision": "c8a5b9c1054985d21d50",
+    "url": "/fabiojsousa/simple-app/static/js/runtime-main.3d7783e6.js"
+  },
+  {
+    "revision": "10f7239221db8d0d2d6f51bfbb5b05b2",
+    "url": "/fabiojsousa/simple-app/static/media/ic-search-copy.10f72392.svg"
+  }
+]);
